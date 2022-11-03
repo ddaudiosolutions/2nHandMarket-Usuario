3809cccc25721517f56a8a4a87ef88fe3d5f8bd7
David Cladera <ddaudiosolutions@gmail.com>
2022-11-03 14:24:29 +0100

    fix adsense

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,16 +1,12 @@
 import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import {
-  obtenerProductosAction,
-  //obtenerCategoriaActions,
-  obtenerPaginaAction,
-} from "../actions/productoActions";
+import {   obtenerProductosAction, obtenerPaginaAction} from "../actions/productoActions";
 import { obtenerBuscoPostsActions } from "../actions/buscoPostActions";
-
 import FormBusqueda from './FormBusqueda'
 import ListaProductos from "./ListaProductos";
 import ListadoPosts from "./ListadoPosts";
+import GoogleAds from "./adsense/GoogleAds";
 
 import "./Producto.css";
 
@@ -66,53 +62,17 @@ const Productos = () => {
 
               <div className="col col-md-9 col-lg-9 mx-auto bg-form mt-5 ">
                 <FormBusqueda busquedaquery={busquedaquery}/>
-                {/* <form>
-                  <div className="container">
-                    <select
-                      className="form-select col-6"
-                      defaultValue={busquedaquery}
-                      name="busqueda"
-                      //SELECCION DE CATEGORIA DE PRODUCTOS A MOSTRAR en REACT MEJOR CON HISTORY.PUSH
-                      onChange={(e) =>
-                        history.push(
-                          `/productos?busqueda=${e.target.value}&page=0`
-                        )
-                      }
-                    >
-                      <option value="ultimos_productos">
-                        Últimos Productos
-                      </option>
-                      <option value="tablas">Tablas</option>
-                      <option value="velas">Velas</option>
-                      <option value="botavaras">Botavaras</option>
-                      <option value="mastiles">Mastiles</option>
-                      <option value="accesorios">Accesorios</option>
-                    </select>
-                  </div>
-                </form> */}
+                
               </div>
             </div>
           </div>
-          <div className="col mx-auto">
-            {/* <div className="row row-cols-2 row-cols-xs-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-5 g-2 justify-content-center "> */}
-              <ListaProductos productos={productos} />
-
-              {/* {!productos
-                ? null
-                : productos.map((producto, busqueda) => (
-                    <Producto
-                      key={producto._id}
-                      producto={producto}
-                      busqueda={busqueda}
-                    />
-                  ))} */}
-            {/* </div> */}
+          <div className="col mx-auto">            
+              <ListaProductos productos={productos} />              
           </div>
           
           <div className="d-flex justify-content-center mt-4 ">
             {busquedaquery !== "ultimos_productos"
-              ? 
-              // <button className="btn btn-primary" onclick={masProductos}>Mostrar más Productos</button> 
+              ?              
               paginas.map((pagina) => (
                   <Link
                     type="submit"
@@ -125,7 +85,7 @@ const Productos = () => {
                 ))
               : null}
           </div>
-
+               <GoogleAds />
           <div className="col mx-auto mt-4 mb-2">
             {busquedaquery === "ultimos_productos" ? (
               <Fragment>
@@ -138,17 +98,7 @@ const Productos = () => {
                 </div>
 
                 <div className="col-12 mx-auto">
-                  <ListadoPosts buscoPosts={buscoPosts} />
-                  {/* <div className="row row-cols-2 row-cols-xs-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-5 g-2 justify-content-center "> */}
-                    {/* {!buscoPosts
-                      ? null
-                      : buscoPosts.map((buscoPost) => (
-                          <BuscoPost
-                            key={buscoPost._id}
-                            buscoPost={buscoPost}
-                          />
-                        ))} */}
-                  {/* </div> */}
+                  <ListadoPosts buscoPosts={buscoPosts} />                  
                 </div>
               </Fragment>
             ) : null}
