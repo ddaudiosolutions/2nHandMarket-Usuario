e154df55c4337ca0b56c2e2144f7bb89b185924e
David_cladera <dcladera@externos.itnow.es>
2024-05-09 10:24:08 +0200

    fix url webcam pipeline

--- src/components/webCams/CanPastilla.jsx
+++ src/components/webCams/CanPastilla.jsx
@@ -1,9 +1,10 @@
 const CanPastilla = () => {
+  const parentDomain = 'localhost' || 'www.mallorcapipeline.com';
   return (
     <div className='col-3 me-3'>
       <h5>Can Pastilla</h5>
       <iframe
-        src={`https://player.twitch.tv/?channel=pipelinesurfshop&height=600&parent=${process.env.REACT_APP_PARENT_DOMAIN}&referrer=https%3A%2F%2Fwww.mallorcapipeline.com%2Fwebcam%2F&width=100%25`}
+        src={`https://player.twitch.tv/?channel=pipelinesurfshop&height=600&parent=${parentDomain}&referrer=https%3A%2F%2Fwww.mallorcapipeline.com%2Fwebcam%2F&width=100%25`}
         width='320'
         height='181'
       ></iframe>
